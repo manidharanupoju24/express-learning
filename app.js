3fba3aa16fe6010681da21d05fdad6ded5ba7323
manidhar.anupoju <manidhar.anupoju@kwench.in>
2022-06-30 11:48:00 +0530

    post put and delete

--- app.js
+++ app.js
@@ -7,12 +7,63 @@ let {people} = require('./data')
 app.use(express.static('./methods-public'))
 //parse form data
 app.use(express.urlencoded({extended: false}))
-
+//parse json
+app.use(express.json())
 
 app.get('/api/people',(req,res)=>{
     res.status(200).json({success:true, data:people})
 })
 
+app.post('/api/people',(req,res)=>{
+    const { name } = req.body
+    if(!name){
+        return res.status(400).json({success: false, msg: 'Please provide name'})
+    }
+     res.status(201).json({success: true, person: name})
+})
+
+app.post('/api/postman/people',(req,res)=>{
+    const {name} = req.body
+    if(!name){
+        return res.status(400).json({success: false, msg: 'Please provide name'})
+    }
+    res.status(201).json({success: true, data: [...people, name]})
+})
+
+app.put('/api/people/:id',(req,res)=>{
+    const {id} = req.params
+    const {name} = req.body
+    //console.log(id, name)
+    //res.send('hello world')
+
+    const person = people.find((person)=> person.id === Number(id))
+    if(!person){
+        return res.status(404).json({success: false, msg: `no person with ${id}`})
+    }
+
+    const newPeople = people.map((person)=> {
+        if(person.id === Number(id)){
+            person.name = name
+        }
+        return person
+    })
+    res.status(200).json({success:true, data: newPeople})
+
+})
+
+app.delete('/api/people/:id',(req,res)=>{
+    //const {id} = req.params
+    const {name} = req.body
+
+    const person = people.find((person)=> person.id === Number(req.params.id))
+    if(!person){
+        return res.status(404).json({success: false, msg: `no person with ${req.params.id}`})
+    }
+
+    const newPeople = people.filter((person)=> person.id !== Number(req.params.id))
+    return res.status(200).json({success:true, data: newPeople})
+    
+})
 app.post('/login',(req,res)=>{
     //console.log(req.body)
     const {name} = req.body 
